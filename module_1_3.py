a270bb29a8c619bfd720c1a418be5493d5989c48
YellingPinetree <yellingpinetree@gmail.com>
2024-11-12 20:58:52 +0700

    Add files via upload
    Module homework

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+Name = 'Elena'
+print (Name, type(Name))
+Age = 23
+print (Age, type(Age))
+New_age  = (Age + 1)
+print (New_age, type(New_age))
+Is_student = True
+print (Is_student, type(Is_student))
+#ActualHomeworkCode
+print ('Name:', Name)
+print ('Age:', Age)
+print ('New age:', New_age)
+print ('Is student:', Is_student)
